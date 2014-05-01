80cbec6a39b33e0adec94b478dbcce72a708c21b
Dagoland <dagoland7@gmail.com>
2014-05-01 13:50:38 -0300

    Update compiz-mate.sh

--- compiz-mate.sh
+++ compiz-mate.sh
@@ -42,9 +42,13 @@ wget -c -O emerald-themes.deb http://goo.gl/gcC4wl
 
 dpkg -i emerald-themes.deb; rm emerald-themes.deb
 
-##### Tema do Mac para emerald
+##### Tema T-ish+Metal para emerald (http://goo.gl/9LTISp)
 
-cd /usr/share/emerald/themes; wget -c -O Mac.tar.bz2 http://gnome-look.org/CONTENT/content-files/137977-T-ish%20Metal%201.0.tar.bz2; tar -jxvf Mac.tar.bz2; mv T-ish\ Metal\ 1.0 T-ish_Metal_1.0; rm Mac.tar.bz2; cd -
+cd /usr/share/emerald/themes
+wget -c -O Mac.tar.bz2 http://gnome-look.org/CONTENT/content-files/137977-T-ish%20Metal%201.0.tar.bz2
+tar -jxvf Mac.tar.bz2
+mv T-ish\ Metal\ 1.0 T-ish_Metal_1.0
+rm Mac.tar.bz2; cd -
 
 ##### Efeito onda única
 
