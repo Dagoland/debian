c740b9eb60bddd7b6fbdae6b586b164fb57b4fc4
Dagoland <dagoland7@gmail.com>
2015-08-21 15:51:05 -0300

    Create exvimon.sh
    Script auxiliar para deixar a cor do painel do mate de cor preta, para ficar semelhante a ao painel do gnome.

---
+++ exvimon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gsettings set org.mate.interface gtk-theme 'Zukitwo'
+
+sleep 30;gsettings set org.mate.interface gtk-theme 'Dagolandy'
+
+# gsettings set org.mate.interface icon-theme 'Numix-Circle'
+
+gsettings set org.mate.peripherals-mouse cursor-theme 'Dagoland-Cursors'
+
+gsettings set org.mate.Marco.general theme 'Dagolandy'
+
+gsettings set org.mate.Marco.general button-layout 'close,minimize,maximize:menu'
